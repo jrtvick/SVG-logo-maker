a203b59c17f167990d9deec332f792e088252723
Tyler Vick <jrtvick@gmail.com>
2023-05-30 18:26:53 -0400

    rendered SVG based on user's answers

--- inquirer.js
+++ inquirer.js
@@ -1,5 +1,6 @@
 const inquirer = require("inquirer");
 const SVG = require("./svg");
+const { Circle, Square, Triangle } = require("./shape");
 
 class CLI {
   init() {
@@ -19,16 +20,32 @@ class CLI {
           type: "list",
           name: "logoShape",
           message: "What shape do you want your logo to be?",
-          choices: ["circle", "square", "triangle"]
+          choices: ["circle", "square", "triangle"],
         },
         {
           type: "input",
           name: "shapeColour",
           message: "What colour do you want your shape to be? (as a hex code)",
-        }
+        },
       ])
       .then((data) => {
-        console.log(data);
+        let shape;
+        switch (data.logoShape) {
+          case "circle":
+            shape = new Circle();
+            break;
+          case "square":
+            shape = new Square();
+            break;
+          case "triangle":
+            shape = new Triangle();
+            break;
+        }
+        shape.setColour(data.shapeColour);
+        const svg = new SVG();
+        svg.setText(data.text, data.textColour);
+        svg.setShape(shape);
+        console.log(svg.render());
       });
   }
 }
--- svg.js
+++ svg.js
@@ -1,17 +1,17 @@
 class SVG {
-    constructor() {
+  constructor() {
     this.text = "";
     this.shape = "";
-    }
-    setText(text, textColour) {
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColour}">${text}</text>`;
-    }
-    setShape(logoShape) {
-        this.shape = logoShape.render()
-    }
-    render() {
-        return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shape}${this.text}</svg>`;
-    }
+  }
+  setText(text, textColour) {
+    this.text = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColour}">${text}</text>`;
+  }
+  setShape(logoShape) {
+    this.shape = logoShape.render();
+  }
+  render() {
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shape}${this.text}</svg>`;
+  }
 }
 
 module.exports = SVG;
