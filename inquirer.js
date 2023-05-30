55a4ed1e8b13dc2911fb41e33b1c594e4161802d
Tyler Vick <jrtvick@gmail.com>
2023-05-30 17:52:25 -0400

    finished the SVG class

--- inquirer.js
+++ inquirer.js
@@ -6,9 +6,25 @@ class CLI {
       .prompt([
         {
           type: "input",
-          name: "logoText",
+          name: "text",
           message: "Input a 1-3 character title for your logo",
         },
+        {
+          type: "input",
+          name: "textColour",
+          message: "What colour do you want your font to be? (as a hex code)",
+        },
+        {
+          type: "list",
+          name: "logoShape",
+          message: "What shape do you want your logo to be?",
+          choices: ["circle", "square", "triangle"]
+        },
+        {
+          type: "input",
+          name: "shapeColour",
+          message: "What colour do you want your shape to be? (as a hex code)",
+        }
       ])
       .then((data) => {
         console.log(data);
